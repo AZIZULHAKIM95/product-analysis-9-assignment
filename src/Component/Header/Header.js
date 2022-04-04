255139f6f510e9288102fb087e52f18ecdfbec1b
AZIZULHAKUM95 <kanon.shaharear@gmail.com>
2022-04-05 05:39:15 +0600

    home reload problem readme file

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import CustomLink from '../CustomLink/CustomLink';
 
 const Header = () => {
     return (
         <div>
-            <nav>
-                <Link to="/">Home</Link>
-                <Link to="/reviews">Reviews</Link>
-                <Link to="/dashboard">Dashboard</Link>
-                <Link to="/blogs">Blogs</Link>
-                <Link to="/about">About</Link>
+            <br />
+            <nav className='d-flex justify-content-evenly fw-bold container fs-6'>
+                <CustomLink to="/">HOME</CustomLink>
+                <CustomLink to="/reviews">REVIEWS</CustomLink>
+                <CustomLink to="/dashboard">DASHBOARD</CustomLink>
+                <CustomLink to="/blogs">BLOGS</CustomLink>
+                <CustomLink to="/about">ABOUT</CustomLink>
             </nav>
 
 
--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import pic from "../Image/jkgf.png"
 
 const Home = () => {
     return (
-        <div>
+        <div className='d-flex m-5'>
+            <div className='m-5'>
+                <h1 className='text-primary fw-bold fs-1 '>WELCOME TO <span className='text-secondary'>SHADOW COMPUTER</span> </h1>
 
+                <h1 className='fw-bold text-primary'>YOUR BEST DEALS IS HERE</h1>
+
+                <p className='fs-4 '>We were honored to receive 38 awards this year—with two products making the “7 most impressive laptops” list.</p>
+
+                <br />
+
+                <Button className='p-3' variant="primary">LIVE DEMO</Button>
+
+            </div>
+            <div className='mb-5'>
+                <img className='w-100 h-100' src={pic} />
+            </div>
         </div>
     );
 };
